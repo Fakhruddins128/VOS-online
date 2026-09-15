6093c9fa4b3e3e3a337dac3527f6ef0e9c02f490
Fakhruddins128 <fakhruddins128@gmail.com>
2026-09-15 16:54:04 +0500

    Update Sidebar.jsx

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -52,7 +52,7 @@ const Sidebar = ({ isOpen, toggleSidebar }) => {
         <div className="sidebar-footer">
           <div className="user-info" aria-label="User Info">
             <div className="user-details">
-              <div className="user-name">version 1.1.0</div>
+              <div className="user-name">Version - 1.1.0</div>
               <div className="user-role">Team IT - 2026-27</div>
             </div>
           </div>
